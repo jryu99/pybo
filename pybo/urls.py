c48d78b7a8b9a82e93d8a02149b56083c7f085a2
DESKTOP-4VAHI79\류지하 <jyou9907@hotmail.com>
2024-08-03 11:29:31 +0900

    파이보 최초 커밋

---
+++ pybo/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from pybo import views
+
+app_name = 'pybo'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+]
